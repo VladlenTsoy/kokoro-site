6354d2c7c79972d140810287b3e9a805573d0e9f
vladlen <vladlengg@gmail.com>
2024-05-23 18:09:51 +0500

    add title for banner

--- src/components/banner/Banner.tsx
+++ src/components/banner/Banner.tsx
@@ -7,6 +7,7 @@ const Banner = () => {
         <div className={styles.banner}>
             <div className={styles.banner_image}>
                 <Image src="/images/banner-tmp.png" alt="banner" objectFit="contain" layout="fill" priority={false} />
+                <h1 className={styles.banner_title}>FEEL YOUR <br/> KOKORO</h1>
             </div>
         </div>
     )
