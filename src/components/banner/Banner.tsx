1c560a0b13ecfa38ed7477c3f7bd187f3d4b410c
vladlentsoy <vladlengg@gmail.com>
2024-05-27 01:22:30 +0500

    update header optimization styles

--- src/components/banner/Banner.tsx
+++ src/components/banner/Banner.tsx
@@ -1,13 +1,18 @@
 import React from "react"
 import styles from "./Banner.module.css"
 import Image from "next/image"
+import BannerSocial from "@/components/banner-social/BannerSocial"
 
 const Banner = () => {
     return (
         <div className={styles.banner}>
             <div className={styles.banner_image}>
-                <Image src="/images/banner-tmp.png" alt="banner" objectFit="contain" layout="fill" priority={false} />
-                <h1 className={styles.banner_title}>FEEL YOUR <br/> KOKORO</h1>
+                <Image src="/images/banner-tmp.png" alt="banner" fill priority={false} />
+                <h1 className={styles.banner_title}>FEEL YOUR <br /> KOKORO</h1>
+                <div className={styles.banner_socials}>
+                    <BannerSocial name="Instagram" link="https://www.instagram.com/kokoro.mode/" />
+                    <BannerSocial name="Telegram" link="https://t.me/kokoro_wear" />
+                </div>
             </div>
         </div>
     )
