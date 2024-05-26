e22a07e81c374a9e987373b6f766aed7f31533ed
vladlentsoy <vladlengg@gmail.com>
2024-05-27 01:58:08 +0500

    change attribute to icon for mobile menu

--- src/layouts/header/header-menu.tsx
+++ src/layouts/header/header-menu.tsx
@@ -12,9 +12,9 @@ const HeaderMenu = () => {
             </div>
             <div className={styles.m_menu}>
                 <svg width="40" height="41" viewBox="0 0 40 41" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M11 15.5H29" stroke="#292D32" stroke-width="1.5" stroke-linecap="round" />
-                    <path d="M11 20.5H29" stroke="#292D32" stroke-width="1.5" stroke-linecap="round" />
-                    <path d="M11 25.5H29" stroke="#292D32" stroke-width="1.5" stroke-linecap="round" />
+                    <path d="M11 15.5H29" stroke="#292D32" strokeWidth="1.5" strokeLinecap="round" />
+                    <path d="M11 20.5H29" stroke="#292D32" strokeWidth="1.5" strokeLinecap="round" />
+                    <path d="M11 25.5H29" stroke="#292D32" strokeWidth="1.5" strokeLinecap="round" />
                 </svg>
             </div>
         </>
