74d3e1405074e998f797209ea8d518d3571556ea
vladlentsoy <vladlengg@gmail.com>
2024-06-10 01:40:02 +0500

    banner for table

--- src/components/banner/Banner.tsx
+++ src/components/banner/Banner.tsx
@@ -3,12 +3,16 @@ import styles from "./Banner.module.css"
 import Image from "next/image"
 import BannerSocial from "@/components/banner-social/BannerSocial"
 
-const Banner = () => {
+interface BannerProps {
+    title: React.ReactNode
+}
+
+const Banner:React.FC<BannerProps> = ({title}) => {
     return (
         <div className={styles.banner}>
             <div className={styles.banner_image}>
                 <Image src="/images/banner-tmp.png" alt="banner" fill priority={false} />
-                <h1 className={styles.banner_title}>FEEL YOUR <br /> KOKORO</h1>
+                <h2 className={styles.banner_title}>{title}</h2>
                 <div className={styles.banner_socials}>
                     <BannerSocial name="Instagram" link="https://www.instagram.com/kokoro.mode/" />
                     <BannerSocial name="Telegram" link="https://t.me/kokoro_wear" />
