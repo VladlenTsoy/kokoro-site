8226fd9fb2420704061f3de33eedb7e8df6de46f
vladlentsoy <vladlengg@gmail.com>
2024-06-16 01:30:00 +0500

    update page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,6 @@ import React from "react"
 import Banner from "@/components/banner/Banner"
 import ProductList from "@/features/product/ProductList"
 import ProductBanner from "@/features/product-banner/ProductBanner"
-import Footer from "@/layouts/footer/Footer"
 
 export default function Home() {
     return (
