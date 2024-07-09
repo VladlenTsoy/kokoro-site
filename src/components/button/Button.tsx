2afefc0f128ac2d58727b8c70672da74557f9e0c
vladlentsoy <vladlengg@gmail.com>
2024-07-10 03:49:22 +0500

    add class block to button

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -1,14 +1,16 @@
 import React, {MouseEventHandler} from "react"
 import styles from "./Button.module.css"
+import cn from "classnames"
 
 interface ButtonProps {
-    onClick: MouseEventHandler<HTMLButtonElement>,
+    onClick?: MouseEventHandler<HTMLButtonElement>,
     children?: React.ReactNode
+    block?: boolean
 }
 
-const Button: React.FC<ButtonProps> = ({children, onClick}) => {
+const Button: React.FC<ButtonProps> = ({children, onClick, block}) => {
     return (
-        <button className={styles.button} onClick={onClick}>
+        <button className={cn(styles.button, {[styles.block]: block})} onClick={onClick}>
             {children}
         </button>
     )
