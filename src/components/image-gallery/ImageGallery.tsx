63c3aa1951710ef49e53ac86e78c1ed9d3f00e1a
vladlentsoy <vladlengg@gmail.com>
2024-07-16 03:31:59 +0500

    update details product page

--- src/components/image-gallery/ImageGallery.tsx
+++ src/components/image-gallery/ImageGallery.tsx
@@ -4,18 +4,57 @@ import React from "react"
 import styles from "./ImageGallery.module.css"
 import Image from "next/image"
 import cn from "classnames"
+import {CarouselProvider, Slider, Slide, Dot} from "pure-react-carousel"
+import {useScreenSize} from "@/hooks/useScreenSize"
+import ImageBlock from "@/components/image-block/ImageBlock"
 
 interface ImageGalleryProps {
-    images: string[]
+    images: {
+        id: number
+        url: string
+    }[]
 }
 
 const ImageGallery: React.FC<ImageGalleryProps> = ({images}) => {
+    const {width} = useScreenSize()
+
+    if (width <= 992)
+        return <div className={styles.sliders}>
+            <CarouselProvider
+                naturalSlideHeight={326}
+                naturalSlideWidth={326}
+                totalSlides={images.length}
+                visibleSlides={1}
+                step={1}
+                dragStep={1}
+                infinite
+            >
+                <Slider>
+                    {
+                        images.map((image, key) =>
+                            <Slide index={image.id} key={key}>
+                                <div className={styles.slider}>
+                                    <ImageBlock src={image.url} alt={`image-${image.id}`} priority={key === 0} />
+                                </div>
+                            </Slide>
+                        )
+                    }
+                </Slider>
+                <div className={styles.dots}>
+                    {
+                        images.map((img, key) =>
+                            <Dot slide={key} className={styles.dot} key={key} />
+                        )
+                    }
+                </div>
+            </CarouselProvider>
+        </div>
 
     return (
         <div className={styles.container}>
             {images.map((image, key) =>
                 <div className={cn(styles.image, {[styles.active]: key === 0})} key={key}>
-                    <Image src={image} alt={`image-${key}`} fill priority={key === 0} />
+                    <Image src={image.url} alt={`image-${key}`} fill priority={key === 0} />
                 </div>
             )}
         </div>
