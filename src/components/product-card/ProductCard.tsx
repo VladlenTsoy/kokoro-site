81704e1552e3b5d16e092f68eb7fd6a5287d0641
vladlentsoy <vladlengg@gmail.com>
2026-02-17 16:27:28 +0500

    update card menu

--- src/components/product-card/ProductCard.tsx
+++ src/components/product-card/ProductCard.tsx
@@ -5,6 +5,7 @@ import {formatPrice} from "@/utils/formatPrice"
 import getTextValue from "@/utils/getTextValue"
 import ImageBlock from "@/components/image-block/ImageBlock"
 import {calculateDiscountedTotal} from "@/utils/calculateDiscountedTotal"
+import cn from "classnames"
 
 interface ProductCardProps {
     id: number
@@ -24,12 +25,19 @@ const ProductCard: React.FC<ProductCardProps> = ({id, title, price, image, disco
                     <ImageBlock src={image} alt={safeTitle} priority={false} fill />
                 </div>
                 <div className={styles.title}>{safeTitle}</div>
+                <div className={cn(styles.price, {[styles.discount]: !!discount})}>
+                    {formatPrice(discount ? calculateDiscountedTotal(price, discount) : price)} сум
+                </div>
                 {
-                    discount && <div className={styles.discount}>
-                        {formatPrice(discount ? calculateDiscountedTotal(price, discount) : price)} сум
+                    discount && <div className={styles.old_price_block}>
+                        <svg width="104" height="18" viewBox="0 0 104 18" fill="none"
+                             xmlns="http://www.w3.org/2000/svg">
+                            <path d="M1 17C19.1615 11.1536 64.9876 -0.210397 103 1.10505" stroke="#F04438"
+                                  strokeWidth="2" strokeLinecap="round" />
+                        </svg>
+                        <div className={styles.old_price}>{formatPrice(price)} сум</div>
                     </div>
                 }
-                <div className={styles.price}>{formatPrice(price)} сум</div>
             </div>
         </Link>
     )
