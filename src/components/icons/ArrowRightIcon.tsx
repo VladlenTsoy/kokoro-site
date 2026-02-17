486e6a463e450f1f54f132d8e0e8c68af117f7e4
vladlentsoy <vladlengg@gmail.com>
2026-02-17 14:21:49 +0500

    add icons

---
+++ src/components/icons/ArrowRightIcon.tsx
@@ -0,0 +1,14 @@
+import React from "react"
+
+const ArrowRightIcon = () => {
+    return (
+        <svg width="28" height="28" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M20 7.90674L28.0933 16.0001L20 24.0934" stroke="white" strokeWidth="2"
+                  strokeMiterlimit="10" strokeLinecap="square" strokeLinejoin="round" />
+            <path d="M4.66406 16H27.1041" stroke="white" strokeWidth="2" strokeMiterlimit="10"
+                  strokeLinecap="square" strokeLinejoin="round" />
+        </svg>
+    )
+}
+
+export default ArrowRightIcon
---
+++ src/components/icons/CloseIcon.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+const CloseIcon = () => {
+    return (
+        <svg width="44" height="44" viewBox="0 0 44 44" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M16.8118 27.1883L27.1884 16.8116" stroke="#5C5C5C" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+            <path d="M27.1884 27.1883L16.8118 16.8116" stroke="#5C5C5C" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+        </svg>
+    )
+}
+
+export default CloseIcon
