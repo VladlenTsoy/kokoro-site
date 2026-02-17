45f0d2e6906730f2003fbe3579f4694f3d86d575
vladlentsoy <vladlengg@gmail.com>
2026-02-17 14:21:16 +0500

    ProductCard.tsx

--- src/components/product-card/ProductCard.tsx
+++ src/components/product-card/ProductCard.tsx
@@ -1,20 +1,20 @@
 import React from "react"
 import styles from "./ProductCard.module.css"
-import Image from "next/image"
 import Link from "next/link"
 import {formatPrice} from "@/utils/formatPrice"
 import getTextValue from "@/utils/getTextValue"
 import ImageBlock from "@/components/image-block/ImageBlock"
+import {calculateDiscountedTotal} from "@/utils/calculateDiscountedTotal"
 
 interface ProductCardProps {
     id: number
     title: string
     price: number
     image: string
-    discountedPrice?: number
+    discount?: number
 }
 
-const ProductCard: React.FC<ProductCardProps> = ({id, title, price, image, discountedPrice}) => {
+const ProductCard: React.FC<ProductCardProps> = ({id, title, price, image, discount}) => {
     const safeTitle = getTextValue(title)
 
     return (
@@ -24,6 +24,11 @@ const ProductCard: React.FC<ProductCardProps> = ({id, title, price, image, disco
                     <ImageBlock src={image} alt={safeTitle} priority={false} fill />
                 </div>
                 <div className={styles.title}>{safeTitle}</div>
+                {
+                    discount && <div className={styles.discount}>
+                        {formatPrice(discount ? calculateDiscountedTotal(price, discount) : price)} сум
+                    </div>
+                }
                 <div className={styles.price}>{formatPrice(price)} сум</div>
             </div>
         </Link>
